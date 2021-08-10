7eee8d9dd2e7a82c1d8967a9d41fb7686784e0c0
Mihai Cvasnievschi <mihai.cvasnievschi@gmail.com>
2021-08-10 11:36:56 +0300

    1st App

---
+++ detector/myapp1/src/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.16
+
+require github.com/r2d2-ai/aiflow v0.0.2
---
+++ detector/myapp1/src/imports.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	_ "github.com/r2d2-ai/aiflow/common/activity/log"
+	_ "github.com/r2d2-ai/aiflow/common/trigger/ipcam"
+	_ "github.com/r2d2-ai/aiflow/flow"
+)
