0a1ae93d221839631aa0b80255d7c2318608f660
Mihai Cvasnievschi <mihai.cvasnievschi@gmail.com>
2021-08-11 09:57:08 +0300

    Cleanup

--- detector/src/go.mod
+++ detector/src/go.mod
@@ -2,4 +2,6 @@ module main
 
 go 1.16
 
-require github.com/r2d2-ai/aiflow v0.0.3
+require github.com/r2d2-ai/aiflow v0.1.0
+
+replace github.com/r2d2-ai/aiflow => ../../../aiflow
--- detector/src/imports.go
+++ detector/src/imports.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	_ "github.com/r2d2-ai/aiflow/common/activity/log"
-	_ "github.com/r2d2-ai/aiflow/common/trigger/ipcam"
-	_ "github.com/r2d2-ai/aiflow/flow"
+	_ "github.com/r2d2-ai/aiflow/action/flow"
+	_ "github.com/r2d2-ai/aiflow/activity/common/log"
+	_ "github.com/r2d2-ai/aiflow/activity/net/mqtt"
+	_ "github.com/r2d2-ai/aiflow/trigger/net/ipcam"
 )
