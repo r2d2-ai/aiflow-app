6da600a2411310a083b4be6fa0ac3dcf1434d79d
Mihai Cvasnievschi <mihai.cvasnievschi@gmail.com>
2021-08-24 04:34:52 +0300

    Subflows

--- detector/src/imports.go
+++ detector/src/imports.go
@@ -3,8 +3,8 @@ package main
 import (
 	_ "github.com/r2d2-ai/aiflow/action/flow"
 	_ "github.com/r2d2-ai/aiflow/action/flow/activity/subflow"
+	_ "github.com/r2d2-ai/aiflow/activity/common/actreturn"
 	_ "github.com/r2d2-ai/aiflow/activity/common/log"
-	_ "github.com/r2d2-ai/aiflow/activity/net/mqtt"
 	_ "github.com/r2d2-ai/aiflow/activity/vision/object_detection"
 	_ "github.com/r2d2-ai/aiflow/activity/vision/transform"
 	_ "github.com/r2d2-ai/aiflow/function/utils"
